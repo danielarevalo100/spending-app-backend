53f177727cf031360c41a35f0daa0f161383c658
yoredut <yoryi.pacheco@gmail.com>
2021-04-10 18:57:32 -0300

    fixing routes for admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -22,10 +22,11 @@ router.get('/transactions', checkheadertoken, async (req, res) => {
         foreignField: '_id',
         as: 'user'
         }
-      }
+      },
+      { $sort: {date: -1} },
+      { $limit: 10 }
     ])
       const response = transactions.filter( trans => trans.user.length ).map( trans => ({...trans, user: tools.cleanData(trans.user[0], ['_id','password','type']) } ));
-      console.log( 'user', response )
       res.status(200).json(response)
     }else{
       res.status(201).json('not_authorized')
@@ -35,6 +36,74 @@ router.get('/transactions', checkheadertoken, async (req, res) => {
   }
 })
 
+router.post( '/transactions/changeStatus', checkheadertoken, async (req, res) => {
+  //if( !('tokenData' in req) ) res.status(400);
+
+  const { body: { id, status } } = req;
+  try{
+
+    const trans = await Transactions.findById(id);
+
+    if( Object.keys(trans).length > 0 ){
+      const { amount, userId, type } = trans;
+
+      switch(status){
+        case 'DONE':
+
+            if(type === 'SEND'){
+
+              await Transactions.updateOne(
+                { _id: id },
+                { '$set': { status: 'DONE' } }
+              );
+
+            } else if(type === 'REQUEST'){
+
+               await Transactions.updateOne(
+                { _id: id },
+                { 
+                  '$set': { status: 'DONE' },
+                }
+              );
+
+              //updating user
+              await User.updateOne(
+                { _id: userId },
+                {'$inc': { balance: amount }}
+              );
+            }
+        break;
+
+        case 'CANCELLED':
+
+            if(type === 'REQUEST'){
+              await Transactions.updateOne(
+                { _id: id},
+                { $set: { status: 'CANCELLED' } }
+              )
+            } else if(type === 'SEND'){
+              await Transactions.updateOne(
+                { _id: id },
+                { 
+                  $set: { status: 'CANCELLED' },
+                }
+              );
+              await User.updateOne(
+                { _id: userId },
+                {$inc: { balance: amount }}
+              );
+            }
+          break;
+          default: res.status(201);
+      }
+    }
+    res.status(200).json({ok: true})
+  }catch( e ){
+    res.status(500)
+    console.log('error', e)
+  }
+
+});
 
 function checkheadertoken(req,res,next){
   const auth = req.headers.authorization;
@@ -53,7 +122,7 @@ function checkheadertoken(req,res,next){
   }else{
     next()
   }
-  
+
 }
 
 module.exports = router;
